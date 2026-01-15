cc576b03e9f45d82c2a1a909511db03b832bb63f
Denzel Kipkemoi <denzelkipkemoi@Denzels-MacBook-Pro.local>
2026-01-15 18:45:10 +0300

    fix: Added procedures to the homepage-dashboard-slot

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@
  * connects the app shell to the React application(s) that make up this
  * microfrontend.
  */
-import { getAsyncLifecycle, defineConfigSchema } from '@openmrs/esm-framework';
+import { getAsyncLifecycle, defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
+import { createDashboardLink } from './createDashboardLink';
+import { proceduresDashboardMeta } from './procedures-dashboard.meta';
 
 const moduleName = '@ampath/esm-procedures-app';
 
@@ -14,31 +16,12 @@ const options = {
   moduleName,
 };
 
-/**
- * This tells the app shell how to obtain translation files: that they
- * are JSON files in the directory `../translations` (which you should
- * see in the directory structure).
- */
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
-/**
- * This function performs any setup that should happen at microfrontend
- * load-time (such as defining the config schema) and then returns an
- * object which describes how the React application(s) should be
- * rendered.
- */
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 }
 
-/**
- * This named export tells the app shell that the default export of `root.component.tsx`
- * should be rendered when the route matches `root`. The full route
- * will be `openmrsSpaBase() + 'root'`, which is usually
- * `/openmrs/spa/root`.
- */
 export const root = getAsyncLifecycle(() => import('./root.component'), options);
 
-/**
- * The following are named exports for the extensions defined in this frontend modules. See the `routes.json` file to see how these are used.
- */
+export const proceduresQueueDashboardLink = getSyncLifecycle(createDashboardLink(proceduresDashboardMeta), options);
---
+++ src/procedures-dashboard.meta.ts
@@ -0,0 +1,6 @@
+export const proceduresDashboardMeta = {
+  path: 'procedures',
+  slot: 'procedures-dashboard-slot',
+  title: 'Procedures',
+  basePath: `${window.spaBase}/home`,
+};
